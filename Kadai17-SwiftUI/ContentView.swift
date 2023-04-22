db5057fb187339b22805c86f91d04cf80f04c30c
Ryuga <ribereo.minami@gmail.com>
2023-04-22 21:13:53 +0900

    copy the contents of kadai16

--- Kadai17-SwiftUI/ContentView.swift
+++ Kadai17-SwiftUI/ContentView.swift
@@ -7,17 +7,90 @@
 
 import SwiftUI
 
+struct Fruit: Identifiable, Equatable {
+    let id = UUID()
+    var name: String
+    var isChecked: Bool
+}
+
 struct ContentView: View {
+    @State private var fruits = [
+        Fruit(name: "りんご", isChecked: false),
+        Fruit(name: "みかん", isChecked: true),
+        Fruit(name: "バナナ", isChecked: false),
+        Fruit(name: "パイナップル", isChecked: true)
+    ]
+
+    @State private var editMode: AddItemView.Mode?
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(fruits) { fruit in
+                    ZStack {
+                        HStack{
+                            Image(systemName: "checkmark")
+                                .foregroundColor(fruit.isChecked ? Color.orange : Color.white)
+                            Spacer().frame(width: 15)
+                            Text(fruit.name)
+                            Spacer()
+                        }
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            if let index = fruits.firstIndex(of: fruit) {
+                                fruits[index].isChecked.toggle()
+                            }
+                        }
+                        HStack {
+                            Spacer()
+                            Image(systemName: "info.circle")
+                                .foregroundColor(Color.blue)
+                        }
+                        .onTapGesture {
+                            if let index = fruits.firstIndex(of: fruit) {
+                                editMode = .update(
+                                    fruit: fruits[index],
+                                    didSave: { updatedFruit in
+                                        if let index = fruits.firstIndex(of: fruit) {
+                                            fruits[index] = updatedFruit
+                                        }
+                                        editMode = nil
+                                    },
+                                    didCancel: {
+                                        editMode = nil
+                                    }
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+            .listStyle(.plain)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        editMode = .create(
+                            didSave: { newFruit in
+                                fruits.append(newFruit)
+                                editMode = nil
+                            },
+                            didCancel: {
+                                editMode = nil
+                            }
+                        )
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                    .fullScreenCover(item: $editMode) { mode in
+                        AddItemView(mode: mode)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
